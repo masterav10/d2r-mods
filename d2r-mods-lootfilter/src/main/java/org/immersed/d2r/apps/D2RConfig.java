3ff575be7b21776093dd2abd2ca44f299b4a16f0
Daniel Avila <masterav10@gmail.com>
2022-11-05 12:31:52 -0400

    Initial configuration.

---
+++ d2r-mods-lootfilter/src/main/java/org/immersed/d2r/apps/D2RConfig.java
@@ -0,0 +1,53 @@
+package org.immersed.d2r.apps;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.immersed.d2r.model.CascSettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Main configuration beans for various applications.
+ * 
+ * @author Dan Avila
+ *
+ */
+@Configuration
+public class D2RConfig
+{
+    /**
+     * Settings for my local D2R.
+     * 
+     * @return the settings object.
+     */
+    @Bean
+    public CascSettings settings()
+    {
+        final Path home = Paths.get("C:", "Program Files (x86)", "Diablo II Resurrected");
+        final Path install = home.resolve("Data");
+        final Path mods = home.resolve("mods");
+        final Path settings = Paths.get("C:", "ProgramData", "d2r-mods");
+
+        return new CascSettings.Builder().installation(install)
+                                         .extraction(settings)
+                                         .mods(mods)
+                                         .build();
+    }
+
+    /**
+     * Json object mapper, to be used for reading and writing files.
+     * 
+     * @return
+     */
+    @Bean
+    public ObjectMapper objectMapper()
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+        return mapper;
+    }
+}
